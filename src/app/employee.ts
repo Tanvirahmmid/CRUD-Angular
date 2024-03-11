67bd1a9b00d78762907bdec56966e30b0c182239
Tanvir Ahmmid <106543545+Tanvirahmmid@users.noreply.github.com>
2024-03-11 17:26:24 +0600

    Add files via upload

---
+++ src/app/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee{
+    id: number;
+    name: string;
+    email: string;
+    address: string;
+    phoneNumber: string;
+    imgUrl: string;
+    accountNumber: string;
+}
